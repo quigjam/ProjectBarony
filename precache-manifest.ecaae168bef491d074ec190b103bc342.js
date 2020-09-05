5f75abdf85d1f7c4f927c40a9765b88e1c58ef5f
James Quigley <quigle48@msu.edu>
2020-09-05 10:57:01 -0400

    Updates

---
+++ precache-manifest.ecaae168bef491d074ec190b103bc342.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99eec7548b70a4f4ff3f2820b89cc1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "063f35f65941fc43d4cf",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "24eb396132ea0a02b16b",
+    "url": "/static/css/main.db67773e.chunk.css"
+  },
+  {
+    "revision": "063f35f65941fc43d4cf",
+    "url": "/static/js/2.fcc96bb9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fcc96bb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24eb396132ea0a02b16b",
+    "url": "/static/js/main.e287b5dd.chunk.js"
+  },
+  {
+    "revision": "7021a92e21e043666e45",
+    "url": "/static/js/runtime-main.2e2c1738.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
